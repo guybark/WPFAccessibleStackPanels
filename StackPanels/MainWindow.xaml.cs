7cc6a1a00475dfd94d0937ae9463daf3883639cc
Guy Barker (CELA) <guybark@microsoft.com>
2020-07-26 09:35:09 -0700

    Add project files.

---
+++ StackPanels/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace StackPanels
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    // Create a StackPanel with a custom AutomationPeer, in order for 
+    // it to be exposed through the Control view of the UIA tree.
+    public class AccessibleStackPanel : StackPanel
+    {
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new AccessibleStackPanelAutomationPeer(this);
+        }
+    }
+
+    public class AccessibleStackPanelAutomationPeer : FrameworkElementAutomationPeer
+    {
+        public AccessibleStackPanelAutomationPeer(AccessibleStackPanel owner) :
+            base(owner)
+        {
+        }
+
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            // This control is logically a container for a group of other control.
+            return AutomationControlType.Group;
+        }
+    }
+
+}
